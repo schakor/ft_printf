aa721c9761ab84b38333bb0844c0537d27d88e11
Khan SADIRAC <khsadira@e3r10p5.42.fr>
2018-11-19 16:24:42 +0100

    update s and c working

--- srcs/conv/conv_s.c
+++ srcs/conv/conv_s.c
@@ -6,7 +6,7 @@
 /*   By: schakor <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/19 12:19:54 by schakor           #+#    #+#             */
-/*   Updated: 2018/11/19 12:20:24 by schakor          ###   ########.fr       */
+/*   Updated: 2018/11/19 16:22:41 by khsadira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,14 @@
 
 void		conv_s(t_pf *pf, t_conv *conv, va_list *ap)
 {
-	(void)pf;
-	(void)conv;
-	(void)ap;
+	char	*s;
+	int		i;
+
+	s = (char *)va_arg(*ap, int *);
+	i = ft_strlen(s);
+	if (i > (int)conv->prec)
+		pf->convsize = (int)conv->prec;
+	else
+		pf->convsize = ft_strlen(s);
+	pf->conv_buf = s;
 }
