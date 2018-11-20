86584580e96820c781a289561188f3d726033922
Khan SADIRAC <khsadira@e2r10p3.42.fr>
2018-11-20 15:03:02 +0100

    update s

--- srcs/conv/conv_s.c
+++ srcs/conv/conv_s.c
@@ -6,22 +6,45 @@
 /*   By: schakor <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/19 12:19:54 by schakor           #+#    #+#             */
-/*   Updated: 2018/11/19 16:46:36 by schakor          ###   ########.fr       */
+/*   Updated: 2018/11/20 15:02:07 by khsadira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+static int	max_value(int a, int b)
+{
+	if (a > b)
+		return (a);
+	else
+		return (b);
+}
+
 void		conv_s(t_pf *pf, t_conv *conv, va_list *ap)
 {
 	char	*s;
+	char	*tmp;
 	int		i;
 
 	s = (char *)va_arg(*ap, char *);
-	i = ft_strlen(s);
-	if ((conv->flag & FLAG_PREC) && i > (int)conv->prec)
-		pf->convsize = (int)conv->prec;
+	if (conv->flag & FLAG_PREC)
+		s = ft_strsub(s, 0, conv->prec);
+	i = max_value(ft_strlen(s), conv->width);
+	if ((conv->flag & FLAG_WIDTH) && conv->width > 1)
+	{
+		if (!(tmp = ft_memalloc(i)))
+			return ;
+		if ((conv->flag & FLAG_ZERO))
+			ft_memset(tmp, '0', conv->width - 1);
+		else
+			ft_memset(tmp, ' ', conv->width - 1);
+		if (conv->flag & FLAG_MINUS)
+			ft_memcpy(tmp, s, i);
+		else
+			ft_memcpy(tmp + (i - ft_strlen(s)), s, i);
+	}
 	else
-		pf->convsize = i;
-	pf->conv_buf = s;
+		tmp = ft_strdup(s);
+	pf->conv_buf = tmp;
+	pf->convsize += i;
 }
